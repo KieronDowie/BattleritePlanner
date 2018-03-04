5837573348bf09acac3484e87f948269d9ae401b
= <=>
2018-03-05 01:06:09 +0200

    Added arrows

--- script.js
+++ script.js
@@ -15,17 +15,28 @@ $(document).ready(function(){
 	init();
 	$('.background').mousedown(function(){
 		event.preventDefault();
+		if (event.which == 3)	//Right click down
+		{
+			startArrow();
+		}
+	});
+	$('.background').mouseup(function(){
+		if (event.which == 3)	//Right click down
+		{
+			endArrow();
+		}
 	});
 	$(document).on("contextmenu", ".background", function(e){
-	   return false;
-	});
-	$(document).on("contextmenu", ".counter", function(e){
-		toggleCounter($(this));
 	   return false;
 	});
 	$('.background').mousemove(function(e){
 		mousex = e.clientX-$('.background').offset().left;
 		mousey = e.clientY-$('.background').offset().top;
+		updateArrow();
+		if (event.which == 3)
+		{
+			$('.counter[halfclick=yes]').removeAttr('halfclick');
+		}
 	});
 	$('.clear').click(clearPlaced);	
 	$('.copy').click(function(){
@@ -74,6 +85,79 @@ $(document).ready(function(){
 		
 	});
 });
+var currentArrowx,currentArrowy,currentArrowHeight,currentArrowAngle;
+var drawingArrow = false;
+function startArrow()
+{
+	drawingArrow = true;
+	currentArrowx = mousex;
+	currentArrowy = mousey;
+	currentArrowHeight = 0;
+	var shaft = $('<div class="arrowshaft"><div class="arrowshaftinner"></div></div>');
+	var arrowhead = $('<img class="arrowhead" src="arrowhead.png"/>');
+	var arrow = $('<div class="arrow"></div>');
+	arrow.addClass('currentarrow');
+	arrow.addClass('hidden');
+	arrow.append(shaft);
+	arrow.append(arrowhead);
+	arrow.css('left',currentArrowx);
+	arrow.css('top',currentArrowy);	
+	$('.background').append(arrow);	
+}
+function endArrow()
+{
+	drawingArrow = false;
+	if (currentArrowHeight-$('.currentarrow').find('.arrowhead').height() < 10)
+	{
+		$('.currentarrow').remove();
+	}
+	$('.currentarrow').click(function(){
+		$(this).remove();
+	});
+	$('.currentarrow').attr('angle',currentArrowAngle);
+	$('.currentarrow').removeClass('currentarrow');
+	updatePlaced();
+	savePlaced();
+}
+function updateArrow()
+{
+	if (drawingArrow)
+	{
+		var dist = distanceTo(currentArrowx,currentArrowy,mousex,mousey);		
+		var shaft = $('.currentarrow .arrowshaft');		
+		shaft.css('height',(dist-$('.currentarrow').find('.arrowhead').height())+'px');
+		currentArrowHeight = dist;
+		currentArrowAngle = angleTo(currentArrowx,currentArrowy,mousex,mousey);
+		$('.currentarrow').css('transform','rotate('+(currentArrowAngle-90)+'deg)');
+		if (currentArrowHeight-$('.currentarrow').find('.arrowhead').height() > 10)
+		{
+			$('.currentarrow').removeClass('hidden');
+		}
+	}
+}
+function angleTo(x1,y1,x2,y2)
+{
+	var opposite = y2-y1;
+	var adjacent = x2-x1;
+	var angle =  Math.atan2(opposite, adjacent);
+	angle = angle * (180/Math.PI);
+	return angle;
+}
+function higher(a,b)
+{
+	return (a>b?a:b)
+}
+function lower(a,b)
+{
+	return (a<b?a:b);
+}
+function distanceTo(x1,y1,x2,y2)
+{
+	var a = x2 - x1;
+	var b = y2 - y1;
+	var c = Math.sqrt(a*a + b*b);
+	return c;
+}
 function updatePlaced()
 {
 	placed = [];
@@ -103,11 +187,26 @@ function updatePlaced()
 			placed.push([name,x,y,allystr]);
 		}
 	}
+	var arrows = $('.arrow');
+	for (i=0;i<arrows.length;i++)
+	{
+		var arrow = arrows[i];
+		var x = $(arrow).css('left');
+		var y = $(arrow).css('top');
+		var angle = $(arrow).attr('angle');
+		var height = $(arrow).css('height').replace('px','');
+		placed.push(['arrow',x,y,roundToTwo(angle),height]);
+	}
 };
 function clearPlaced()
 {
 	$('.counter').remove();
+	$('.arrow').remove();
 	updatePlaced();
+}
+function roundToTwo(num)
+{
+	return ( Math.round(num*100) / 100);
 }
 function savePlaced()
 {
@@ -115,7 +214,14 @@ function savePlaced()
 	var arr = [];
 	for (i in placed)
 	{
-		arr.push(placed[i][0]+placed[i][3]+'='+placed[i][1]+'/'+placed[i][2]);
+		if (placed[i][0] == 'arrow')
+		{
+			arr.push(placed[i][0]+'='+placed[i][1]+'/'+placed[i][2]+'/'+placed[i][3]+'/'+placed[i][4]);
+		}
+		else
+		{
+			arr.push(placed[i][0]+placed[i][3]+'='+placed[i][1]+'/'+placed[i][2]);
+		}
 	}
 	arr = arr.join('&');
 	str+='?'+arr;
@@ -134,11 +240,37 @@ function init()
 		{
 			params[i] = [params[i].split('=')[0],params[i].split('=')[1]];
 			var champion = params[i][0].split('+')[0];
-			var location = params[i][1].split('/');
-			var ally = params[i][0].split('+')[1];			
-			addCounter(champion,location[0],location[1],false,ally);
+			
+			if (champion == 'arrow')		
+			{
+				var arr = params[i][1].split('/');
+				var x = arr[0];
+				var y = arr[1];
+				var angle = arr[2];
+				var height = arr[3];
+				addArrow(x,y,angle,height);
+			}
+			else
+			{
+				var location = params[i][1].split('/');
+				var ally = params[i][0].split('+')[1];	
+				addCounter(champion,location[0],location[1],false,ally);
+			}
 		}
 	}
+}
+function addArrow(x,y,angle,height)
+{
+	var shaft = $('<div class="arrowshaft"><div class="arrowshaftinner"></div></div>');
+	var arrowhead = $('<img class="arrowhead" src="arrowhead.png"/>');
+	var arrow = $('<div class="arrow"></div>');
+	arrow.append(shaft);
+	arrow.append(arrowhead);
+	arrow.css('left',x);
+	arrow.css('top',y);
+	$(arrow).css('transform','rotate('+(angle-90)+'deg)');
+	shaft.css('height',(height-$('.currentarrow').find('.arrowhead').height())+'px');
+	$('.background').append(arrow);	
 }
 function addCounter(name,x,y,held,ally)
 {
@@ -159,15 +291,26 @@ function addCounter(name,x,y,held,ally)
 	counter.css('left',x+'px');
 	counter.css('top',y+'px');
 	$('.background').append(counter);
-		updateTracker();
-		$('.counter').removeClass('hidden');
-		$('.counter').mousedown(function(){
-			if (event.which == 1) //Left click down
-			{
-				event.preventDefault();
-				$(this).addClass('tracker');
-			}			
-		});
+	updateTracker();
+	$('.counter').removeClass('hidden');
+	$('.counter').mousedown(function(){
+		if (event.which == 1) //Left click down
+		{
+			event.preventDefault();
+			$(this).addClass('tracker');
+		}
+		else if (event.which == 3)
+		{
+			$(this).attr('halfclick','yes');
+		}
+	});
+	$('.counter').mouseup(function(){
+		if ($(this).attr('halfclick') && event.which == 3)
+		{
+			$(this).removeAttr('halfclick');
+			toggleCounter($(this));
+		}
+	})
 }
 function toggleCounter(counter)
 {
